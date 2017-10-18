61c6aa06115945ff34b2d58cd82315c49df28552
Cortland Mahoney <cortland.mahoney@gmail.com>
2017-10-18 01:25:45 -0400

    Remove Coupon and Product Data

--- themes/KDJ/woocommerce/checkout/review-order.php
+++ themes/KDJ/woocommerce/checkout/review-order.php
@@ -22,27 +22,46 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <div class="woocommerce_totals shop_table woocommerce-checkout-review-order-table">
-	
+
 	<? if ( WC()->cart->coupons_enabled() ) { ?>
 		<div class="cart_discount_code skin">
 			<div class="coupon">
 				<div class="woocommerce-info">Discount Code</div>
 				<? do_action( 'woocommerce_before_checkout_coupon' ); ?>
+				<? foreach ( WC()->cart->get_coupons() as $code => $coupon ): ?>
+                <?php
+                    $type = '&nbsp;';
+                    $_type = $coupon->get_discount_type();
+
+                    if ( strpos('percent', $_type) ){
+                        $type = '%';
+                    } elseif ( strpos('fixed', $_type) ) {
+                        $type = '$';
+                    }
+                ?>
+
+                <div class="cmrd-coupon">
+                    <div class="cmrd-coupon-data">
+                        <em><?= $code ?>:&nbsp;</em><strong><?= $type ?><?= $coupon->amount ?></strong>
+                    </div>
+                    <input type="submit" value="Remove Coupons" class="button remove-coupon" data-code=" <?=$coupon->get_code()?> " />
+                </div>
+            <? endforeach ?>
 			</div>
 		</div>
 	<? } ?>
-	
+
 	<ul id="totals">
 		<li class="cart-subtotal">
 			<span><?php _e( 'Subtotal', 'woocommerce' ); ?></span><span><?php wc_cart_totals_subtotal_html(); ?></span>
 		</li>
-		
+
 		<?php foreach ( WC()->cart->get_fees() as $fee ) : ?>
 			<li class="fee">
 				<span><?php echo esc_html( $fee->name ); ?></span><span><?php wc_cart_totals_fee_html( $fee ); ?></span>
 			</li>
 		<?php endforeach; ?>
-	
+
 		<?php if ( wc_tax_enabled() && 'excl' === WC()->cart->tax_display_cart ) : ?>
 			<?php if ( 'itemized' === get_option( 'woocommerce_tax_total_display' ) ) : ?>
 				<?php foreach ( WC()->cart->get_tax_totals() as $code => $tax ) : ?>
@@ -56,21 +75,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 				</li>
 			<?php endif; ?>
 		<?php endif; ?>
-		
+
 		<?php if ( WC()->cart->needs_shipping() && WC()->cart->show_shipping() ) : ?>
-	
+
 			<?php do_action( 'woocommerce_review_order_before_shipping' ); ?>
-	
+
 			<li class="cart_review_totals_shipping">
 				<span><?php _e( 'Shipping', 'woocommerce' ); ?></span><span class="shipping-amount"><?php echo WC()->cart->get_cart_shipping_total(); ?></span>
 			</li>
-	
+
 			<?php //wc_cart_totals_shipping_html(); ?>
-	
+
 			<?php do_action( 'woocommerce_review_order_after_shipping' ); ?>
-	
+
 		<?php endif; ?>
-		
+
 		<?php foreach ( WC()->cart->get_coupons() as $code => $coupon ) : ?>
 			<li class="cart-discount coupon-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
 			<? $amount = WC()->cart->get_coupon_discount_amount( $coupon->get_code(), WC()->cart->display_cart_ex_tax ) * -1 ?>
@@ -78,50 +97,50 @@ if ( ! defined( 'ABSPATH' ) ) {
 			</li>
 		<?php endforeach; ?>
 
-	
+
 		<?php do_action( 'woocommerce_review_order_before_order_total' ); ?>
-	
+
 		<li class="order-total">
 			<span><?php _e( 'Total', 'woocommerce' ); ?></span><span><?php wc_cart_totals_order_total_html(); ?></span>
 		</li>
-	
+
 		<?php do_action( 'woocommerce_review_order_after_order_total' ); ?>
 	</ul>
 	<?php
 		$featuredProductID = get_field('checkout_and_cart_featured_product_id', $post_id);
 		$preferredFeaturedImage = get_field('preferred_image', $post_id);
-		
+
 		if ($preferredFeaturedImage) {
 			$featuredProductImageURL = $preferredFeaturedImage;
 		} else {
 			$woocommerceFeaturedImageID = get_post_thumbnail_id($featuredProductID);
 			$featuredProductImageURL = trim(wp_get_attachment_image_src($woocommerceFeaturedImageID, 'medium' )[0]);
 		}
-		
+
 		$featuredProduct = wc_get_product( $featuredProductID );
 		$featuredProductURL = get_permalink($featuredProductID);
 		$featuredText = get_field('featured_text', $post_id);
 	?>
-	
+
 	<div class="checkout_and_cart_featured_image" style="background: url(<?=$featuredProductImageURL?>)">
 		<span class="checkout_and_cart_featured_text"><?=$featuredText?></span>
 			<a href="<?=$featuredProductURL?>" class="cta" id="featured_checkout_item" data-product-id="<?=$featuredProductID?>">ADD TO CART</a>
 	</div>
-	
-	
-	
+
+
+
 	<?php do_action( 'woocommerce_review_order_before_cart_contents' );
 
 	foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
 		$_product     = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
-	
+
 		if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_checkout_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
 			?>
 			<ul class="<?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?> checkout_tbl_row">
 				<li class="check_prod_photo_cell">
 	                <?php
 	                $thumbnail = apply_filters('woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key);
-	
+
 	                if (!$_product->is_visible())
 	                    echo $thumbnail;
 	                else
@@ -135,25 +154,25 @@ if ( ! defined( 'ABSPATH' ) ) {
 	                    echo apply_filters('woocommerce_cart_item_name', $_product->get_title(), $cart_item, $cart_item_key);
 	                else
 	                    echo apply_filters('woocommerce_cart_item_name', sprintf('<a href="%s">%s</a>', $_product->get_permalink(), $_product->get_title()), $cart_item, $cart_item_key);
-	
+
 					$product_color = ($cart_item['variation']['attribute_pa_color']) ? strtoupper($cart_item['variation']['attribute_pa_color']) : false;
 					$product_size = ($cart_item['variation']['attribute_pa_size']) ? explode('size-', $cart_item['variation']['attribute_pa_size'])[1] : false;
-					
+
 					if (strlen($product_size) > 1) {
 						$firstSizeNumber = substr($product_size, 0, 1);
 						$product_size = substr_replace($product_size, $firstSizeNumber . "/", 0, 1);
 					}
-					
+
 					$product_color = ($product_color) ? "COLOR : " . $product_color : '';
 					$product_size = ($product_size) ? "<span class='size_txt'>SIZE</span> <span class='size_numbers'>" . $product_size . "</span>": '';
-			
-				
-					
-					
-	
+
+
+
+
+
 	        // Meta data
-	        
-	
+
+
 	        // Backorder notification
 	        if ($_product->backorders_require_notification() && $_product->is_on_backorder($cart_item['quantity']))
 	            echo '<p class="backorder_notification">' . __('Available on backorder', 'woocommerce') . '</p>';
@@ -175,10 +194,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<?php
 		}
 	}
-		
-	do_action( 'woocommerce_review_order_after_cart_contents' ); ?>	
-	
-	
+
+	do_action( 'woocommerce_review_order_after_cart_contents' ); ?>
+
+
 </div>
 
 
