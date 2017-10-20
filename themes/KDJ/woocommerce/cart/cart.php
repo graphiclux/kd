aab2f61a50f1a0c09a6b637f71b16450a56a12d1
Cortland Mahoney <cortland.mahoney@gmail.com>
2017-10-20 00:10:52 -0400

    Update cart.php

--- themes/KDJ/woocommerce/cart/cart.php
+++ themes/KDJ/woocommerce/cart/cart.php
@@ -23,12 +23,15 @@ wc_print_notices();
 
             <?php
             foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
+
+
+
                 $_product = apply_filters('woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key);
                 $product_id = apply_filters('woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key);
 
                 if ($_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters('woocommerce_cart_item_visible', true, $cart_item, $cart_item_key)) {
                     ?>
-
+                    <? print_r( $_product->get_attributes() ); ?>
                     <ul class="checkout_tbl_row <?php echo esc_attr(apply_filters('woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key)); ?>">
                         <li class="check_prod_photo_cell">
                             <?php
@@ -48,73 +51,47 @@ wc_print_notices();
 		                    else
 		                        echo apply_filters('woocommerce_cart_item_name', sprintf('<a href="%s">%s</a>', $_product->get_permalink(), $_product->get_title()), $cart_item, $cart_item_key);
 
-                    // $variations = [];
-                    // foreach($cart_item['variation'] as $key => $variation) {
-                    //     if (empty($variation)) continue;
-
-                    //     $idx = strrpos($key, '_') + 1;
-
-                    //     $key = ucfirst(substr($key, $idx));
-                    //     $variation = ucfirst($variation);
-                    //     $variations[$key] = $variation;
-                    // }
-
-                    //foreach($variations as $key => $var): <!-- ?>
-                    //
-                                        <!-- <span class="cart_product_color"></span> -->
-                 <!--
-                    // endforeach;
-                    ?> -->
-
-                    <?php
-
                     // Meta data
                     // Backorder notification
                     if ($_product->backorders_require_notification() && $_product->is_on_backorder($cart_item['quantity']))
                         echo '<p class="backorder_notification">' . __('Available on backorder', 'woocommerce') . '</p>';
                             ?></h3>
                             <ul>
-                                <?php echo WC()->cart->get_item_data($cart_item); ?>
+                                <?php $variations = $cart_item['variation']; ?>
+                                <?php if ( count($variations) ) : ?>
+                                <?php foreach ($variations as $key => $value) {
+                                    $idx = strrpos($key, 'pa_') + 3;
+                                    $key = strtoupper( substr($key, $idx) );
+
+                                    $value = strtoupper( str_replace( '-', ' ', $value) );
+                                }
+                                ?>
+                                <li><b><?= $key ?></b> : <?= $value ?></li>
+                                <li><b>Qty:</b> <?= $cart_item['quantity']; ?>
+                                <?php endif ?>
                                 <li>$<?php echo $_product->get_price(); ?></li>
+
+                                <?php
+                                    if ( $_product->is_sold_individually() ) {
+                                        $product_quantity = sprintf( '1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key );
+                                    } else {
+                                        $product_quantity = woocommerce_quantity_input( array(
+                                            'input_name'  => "cart[{$cart_item_key}][qty]",
+                                            'input_value' => $cart_item['quantity'],
+                                            'max_value'   => $_product->get_max_purchase_quantity(),
+                                            'min_value'   => '0',
+                                        ), $_product, false );
+                                    }
+
+                                    echo apply_filters( 'woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item );
+                                ?>
                             </ul>
-
-
-
 
 	                            <?
                             echo apply_filters('woocommerce_cart_item_remove_link', sprintf('<a href="%s" class="checkout_delete_link" title="%s">REMOVE</a>', esc_url(WC()->cart->get_remove_url($cart_item_key)), __('Remove this item', 'woocommerce')), $cart_item_key);
                             ?>
                         </li>
 
-
-                        <li class="check_prod_style_cell">
-                        	<?=($product_size) ? '<span class="cart_product_size">'.$product_size.'</span>' : '';?>
-                        	<span class="qty_txt">QTY</span>
-                            <?php
-                            if ($_product->is_sold_individually()) {
-                                $product_quantity = sprintf('1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key);
-                            } else {
-                                $product_quantity = woocommerce_quantity_input(array(
-                                    'input_name' => "cart[{$cart_item_key}][qty]",
-                                    'input_value' => $cart_item['quantity'],
-                                    'max_value' => $_product->backorders_allowed() ? '' : $_product->get_stock_quantity(),
-                                    'min_value' => '0'
-                                        ), $_product, false);
-                            }
-
-                            ?>
-                            <select class="input-text qty text" name="cart[<?php echo $cart_item_key; ?>][qty]">
-                                <?php
-                                for($r=1; $r<=$_product->get_stock_quantity(); $r++){
-                                    echo '<option value="'.$r.'"';
-                                    if($r==$cart_item['quantity']){
-                                        echo ' selected="selected"';
-                                    }
-                                    echo '>'.$r.'</option>';
-                                }
-                                ?>
-                            </select>
-                        </li>
                     </ul>
 
 
